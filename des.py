4db46c748fdb54cc22e44d27b0a8d9e7b5f3b04f
Brandon Nguyen <brandon0180@csu.fullerton.edu>
2019-04-08 20:02:11 -0700

    DES is done I think

--- des.py
+++ des.py
@@ -1,8 +1,46 @@
 from Crypto.Cipher import DES
+from Crypto import Random
+
+global plain
+global cipher
+global done
+global x
+done = False
+plain = ""
+cipher = ""
+
 
 class DES:
     def setKey(self,key):
+        global done,x
+        try:
+            self.key = key
+            if len(self.key) == 8:
+                done = True
+                x = DES.new(self.key,DES.MODE_ECB)
+        except:
+            pass
 
     def encrypt(self,plainText):
+        global cipher, done, x
+        self.plainText = plainText
+
+        while len(self.plainText) % 8 != 0:
+            self.plainText += '*'
+        
+        if Success:
+            cipher = obj.encrypt(self.plainText)
+            return cipher
+        else:
+            print("Invalid Key")
+            return self.plainText
 
     def decrypt(self,cipherText):
+         global plain, done, x
+        self.cipherText = cipherText
+        if Success:
+            plain = obj.decrypt(self.cipherText)
+            return plain
+        else:
+            print("Invalid Key")
+            return self.cipherText
